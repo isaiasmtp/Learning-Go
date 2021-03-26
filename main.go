b86ac1b5606e0aa5b5ce891353aa0384b9dba957
Isaias Pontes <isaias.pontes@ccc.ufcg.edu.br>
2021-03-25 21:01:16 -0300

    Adding main

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	ok, err := say("Hello Word")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	switch ok {
+	case true:
+		fmt.Println("Worked")
+	case false:
+		fmt.Println("Dont Worked")
+	}
+}
+
+func say(what string) (bool, error) {
+	if what == "" {
+		return false, fmt.Errorf("Empty String")
+	}
+
+	fmt.Println(what)
+
+	return true, nil
 }
